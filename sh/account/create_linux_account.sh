6c12826a40bc09cf76ab327664744401020e0550
ein blatt <einblattx@gmail.com>
2025-12-09 14:14:06 +0800

    update: 更新sh

--- sh/account/create_linux_account.sh
+++ sh/account/create_linux_account.sh
@@ -5,14 +5,14 @@ USER="modifyapp"
 HOME_DIR="/usr/local/docker/nginx/app"
 PASSWORD="linux"
 
-echo "正在创建用户 $USER ..."
 useradd -m -d "$HOME_DIR" -s /bin/bash "$USER"
 echo "$USER:$PASSWORD" | chpasswd
 chown -R $USER:$USER $HOME_DIR
-chmod -R 777 "$HOME_DIR"
+chmod -R 755 "$HOME_DIR"
+chmod 755 /usr/local/docker/nginx
 
 
 echo "用户 $USER 创建完成！"
 echo "home目录: $HOME_DIR"
 echo "初始密码: $PASSWORD"
-echo "权限已改为 700"
+echo "权限已改为 755"
